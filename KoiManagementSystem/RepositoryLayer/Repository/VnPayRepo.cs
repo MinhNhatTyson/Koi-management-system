62fdac962045c7e575c2c2e005999ac27d05b982
MinhNhatTyson <minhnhat0132@gmail.com>
2024-10-24 11:53:48 +0700

    Return URL Could be modified

---
+++ KoiManagementSystem/RepositoryLayer/Repository/VnPayRepo.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer;
+using BusinessLayer.Entities;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Repository
+{
+    public class VnPayRepo : IVnPayRepo
+    {
+        private readonly KoiCareContext _context;
+
+        public VnPayRepo(KoiCareContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Order> GetOrderByIdAsync(int orderId)
+        {
+            return await _context.Orders.FindAsync(orderId);
+        }
+    }
+}
